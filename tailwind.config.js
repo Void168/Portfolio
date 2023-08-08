b55d0ded4eb2f4809bd091b0fac0fc95bc90a810
LPHung <phuhung16820@gmail.com>
2023-08-09 03:01:18 +0700

    update projects images

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,10 +37,10 @@ export default {
         project:"url('https://c4.wallpaperflare.com/wallpaper/726/764/940/dark-green-minimalism-geometry-abstraction-wallpaper-preview.jpg')",
         pj1: "url('/sfsff.png')",
         pj2: "url('/Image/projects/chefhoangdung.png')",
-        pj3: "url('./Image/projects/spotify_clone.png')",
+        pj3: "url('https://th.bing.com/th/id/R.b0a44a66d9d41a4693dbf4488b69f0c5?rik=IOtIhpHRfjj6JQ&riu=http%3a%2f%2fmedia.idownloadblog.com%2fwp-content%2fuploads%2f2016%2f02%2fapple-music-android-release-date-app.jpg&ehk=u%2fy1ACVRpUByEfmM%2bHg5U%2fPXiQWWpw9EbRhRwjYsMcM%3d&risl=&pid=ImgRaw&r=0')",
         pj4: "url('https://img.freepik.com/free-vector/restaurant-mural-wallpaper_23-2148704189.jpg')",
         pj5: "url('https://th.bing.com/th/id/OIP.LtR8rD05CLZrCJpGQy23DQHaEK?pid=ImgDet&rs=1')",
-        pj6: "url('./Image/projects/bookingapp.png')"
+        pj6: "url('https://th.bing.com/th/id/R.f43e1ac565de3c3260a5aa724c57b202?rik=Rlw7bAUfF5C5lA&pid=ImgRaw&r=0')"
       },
     },
   },
